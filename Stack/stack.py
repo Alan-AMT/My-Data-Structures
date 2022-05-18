a74592e59b9bbef6d87325eab2e2084c500018b3
Alan Muñoz <105015947+Alan-AMT@users.noreply.github.com>
2022-05-17 19:26:04 -0500

    Update stack.py

--- Stack/stack.py
+++ Stack/stack.py
@@ -43,3 +43,27 @@ class Stack:
             amount += 1
             temp = temp.previous
         return amount
+
+    
+    
+################ SIMPLEST IMPLEMTATION #################
+    class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)				
+
+    def pop(self):
+				if not self.is_empty():
+		        return self.items.pop()
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        
+    def get_stack(self):
+        return self.items
