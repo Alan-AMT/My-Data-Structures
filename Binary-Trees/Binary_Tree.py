d7f9e2fa02e3a78f46fd47544c2c59284e900a51
Alan Muñoz <105015947+Alan-AMT@users.noreply.github.com>
2022-06-21 20:07:44 -0500

    Update Binary_Tree.py

--- Binary-Trees/Binary_Tree.py
+++ Binary-Trees/Binary_Tree.py
@@ -7,27 +7,42 @@ class BinaryTree(BinaryTreeInterface):
         self.right = None
     
     #Traversal functions are meant to return a list with all the roots of the tree
-    #pre/in/post_order_list is an optional parameter if you want to append the roots to a specific list
-    def pre_order(sub_tree, pre_order_list = []):
-        if sub_tree:
-            pre_order_list.append(sub_tree.root)
-            BinaryTree.pre_order(sub_tree.left)
-            BinaryTree.pre_order(sub_tree.right)
-        return pre_order_list
+    def pre_order(main_root):
+        pre_order_values = []
+        
+        def get_pre_order(sub_tree):
+            if sub_tree:
+                get_pre_order(sub_tree.left)
+                pre_order_values.append(sub_tree.root)
+                get_pre_order(sub_tree.right)
+                
+        get_pre_order(main_root)
+        return pre_order_values
             
-    def in_order(sub_tree, in_order_list = []):
-        if sub_tree:
-            BinaryTree.in_order(sub_tree.left)
-            in_order_list.append(sub_tree.root)
-            BinaryTree.in_order(sub_tree.right)
-        return in_order_list
+    def in_order(main_root):
+        in_order_values = []
+        
+        def get_in_order(sub_tree):
+            if sub_tree:
+                get_in_order(sub_tree.left)
+                in_order_values.append(sub_tree.root)
+                get_in_order(sub_tree.right)
+                
+        get_in_order(main_root)
+        return in_order_values
             
-    def post_order(sub_tree, post_order_list = []):
-        if sub_tree:
-            BinaryTree.post_order(sub_tree.left)
-            BinaryTree.post_order(sub_tree.right)
-            post_order_list.append(sub_tree.root)
-        return post_order_list
+    def post_order(main_root):
+        post_order_values = []
+        
+        def get_post_order(sub_tree):
+            if sub_tree:
+                get_post_order(sub_tree.left)
+                get_post_order(sub_tree.right)
+                post_order_values.append(sub_tree.root)
+
+        get_post_order(main_root)
+        return post_order_values
             
     def __str__(self):
-        return self.in_order()
+        print(*self.in_order(), end="")
+        return ""
